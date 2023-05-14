210ecec9eba60dbff3ef8d3d1a3aea7c98facc97
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-05-14 13:19:19 -0400

    Add Kokkos variant

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,8 +1,17 @@
+#include <chrono>
 #include <iostream>
 #include <random>
 
+#include "KokkosRadixSort.hpp"
 #include "OMPRadixSort.hpp"
 #include "SerialRadixSort.hpp"
+
+void print_vector(const std::vector<int> v) {
+  for (auto i : v) {
+    std::cout << i << " ";
+  }
+  std::cout << "\n";
+}
 
 auto main(int argc, char *argv[]) -> int {
   size_t max_digits = 5;
@@ -22,21 +31,39 @@ auto main(int argc, char *argv[]) -> int {
 
   std::generate(input.begin(), input.end(), gen);
 
-  std::vector<const std::string> model_names = {"Serial", "OpenMP"};
+  std::vector<const std::string> model_names = {"Serial", "OpenMP", "Kokkos"};
   RadixSort *models[] = {new SerialRadixSort(max_digits, N, input),
-                         new OMPRadixSort(max_digits, N, input)};
+                         new OMPRadixSort(max_digits, N, input),
+                         new KokkosRadixSort(max_digits, N, input)};
   size_t num_models = model_names.size();
 
-  for (size_t i = 1; i < num_models; i++) {
-    std::cout << model_names[i] << "\n";
+  std::chrono::steady_clock::time_point start;
+  std::chrono::steady_clock::time_point end;
+  std::chrono::duration<double> elapsed_seconds;
+
+  std::vector<std::chrono::duration<double>> timings(num_models);
+
+  for (size_t i = 0; i < num_models; i++) {
+    start = std::chrono::steady_clock::now();
     models[i]->sort();
+    end = std::chrono::steady_clock::now();
+    elapsed_seconds = end - start;
+
+    timings[i] = elapsed_seconds;
+
     std::vector<int> output = models[i]->get_sorted_values();
 
     if (std::is_sorted(output.cbegin(), output.cend())) {
       std::cout << "Data is sorted!\n";
+      // print_vector(output);
     } else {
       std::cout << "Data is not sorted\n";
     }
+  }
+
+  std::cout << "Timings\n";
+  for (int i = 0; i < num_models; i++) {
+    std::cout << model_names[i] << " " << timings[i].count() << "\n";
   }
 
   return 0;
